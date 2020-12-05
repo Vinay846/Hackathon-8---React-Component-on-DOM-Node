ea2af673548124fef8f3f9aed49ba4e581c539b6
Vinay846 <Vinayvin84629@gmail.com>
2020-12-05 17:45:45 +0530

    forgot return

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function App(){
-    render(
+    return(
         <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
     );
 }
